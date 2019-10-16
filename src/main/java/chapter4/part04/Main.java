182a29f6ef911d00fc9a9a1021cf958e1451fa76
Fan Bin <fanbin0602@163.com>
2019-10-16 10:00:20 +0800

    add interface show case main method

--- src/main/java/chapter4/part04/Main.java
+++ src/main/java/chapter4/part04/Main.java
@@ -7,7 +7,15 @@ package chapter4.part04;
 public class Main {
 
     public static void main(String[] args) {
+        Bird bird = new Bird();
+        Plane plane = new Plane();
 
+        letItFly(bird);
+        letItFly(plane);
+    }
+
+    public static void letItFly(Flyer flyer) {
+        flyer.fly();
     }
 
 }
