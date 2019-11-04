aebaba88d7ecdb962d6d8787ec6758aa961a632c
Fan Bin <fanbin0602@163.com>
2019-11-04 20:43:22 +0800

    chapter7.part2

---
+++ src/main/java/chapter7/part02/Main.java
@@ -0,0 +1,24 @@
+package chapter7.part02;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * @author fanbin
+ * @date 2019/11/4
+ */
+public class Main {
+
+    // 字节输入流
+    InputStream inputStream;
+    // 字节输出流
+    OutputStream outputStream;
+
+    // 字符输入流
+    Reader reader;
+    // 字符输出流
+    Writer writer;
+
+}
